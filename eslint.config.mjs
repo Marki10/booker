b3f9fbbcf7402527e00073ded4706447be8b9e2b
Mark Darabos <Marki10@users.noreply.github.com>
2026-02-10 19:56:13 +0000

    Update huskey props

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist", ".next", "out", "node_modules"]),
+  globalIgnores(["dist", ".next", "out", "node_modules", "next-env.d.ts"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
